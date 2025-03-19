5eca043b5f89e1414af0e889b5b352c26120da2e
hema1414 <hemaalpsdeal@gamil.com>
2025-03-19 22:37:40 +0530

    Mapbox concept hide , SAP new gradle file updated MARCH_2025

--- app/src/main/java/com/winapp/saperp/activity/SplashActivity.java
+++ app/src/main/java/com/winapp/saperp/activity/SplashActivity.java
@@ -157,13 +157,14 @@ public class SplashActivity extends AppCompatActivity {
             }else {
 
 //hana app
-               //    String apiUrl=Constants.RAYMANG_URL_JULY;
+              //  String apiUrl=Constants.test_URL_OCT24;
+
+                //    String apiUrl=Constants.RAYMANG_URL_JULY;
                 //  String apiUrl=Constants.FUXIN;
-               // String apiUrl=Constants.TRANS_ORIENT_LIVE;
-                 String apiUrl=Constants.TRANS_ORIENT_DEMO;
+                String apiUrl=Constants.TRANS_ORIENT_LIVE;
+             //   String apiUrl=Constants.TRANS_ORIENT_DEMO;
 
 /*SQL app
-//                String apiUrl=Constants.test_URL_OCT24;
 //          String apiUrl=Constants.AADHI_DEMO;
 //         String apiUrl=Constants.SUPER_STAR_DEMO;
 //          String apiUrl=Constants.thongai_url;
